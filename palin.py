90e9e67a07eb3918f8bd23ada7b6396d63b8c0bc
keerthanachinna <40225685+keerthanachinna@users.noreply.github.com>
2018-07-06 19:05:59 +0530

    Create palin.py

---
+++ palin.py
@@ -0,0 +1,16 @@
+def isPalindrome(str):
+ 
+    
+    for i in xrange(0, len(str)/2): 
+        if str[i] != str[len(str)-i-1]:
+            return False
+    return True
+ 
+
+s = "lappal"
+ans = isPalindrome(s)
+ 
+if (ans):
+    print("Yes")
+else:
+    print("No")
