cefccd283c289b8a63ecdb3cb686fef16c128a28
keerthanachinna <40225685+keerthanachinna@users.noreply.github.com>
2018-10-22 12:35:18 +0530

    Create composite1.c

---
+++ composite1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n=123,fact;
+clrscr();
+for(i=1;i<n;i++)
+{
+if(n%i==0)
+{
+fact==1
+}
+}
+if(fact>1){
+printf("yes");
+}else{
+printf("no");
+}getch();
+}
