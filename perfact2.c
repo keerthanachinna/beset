8a57c2721a6aba9e88e84c9422b79a5a6e1fc047
keerthanachinna <40225685+keerthanachinna@users.noreply.github.com>
2018-10-22 14:20:34 +0530

    Create perfact2.c

---
+++ perfact2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+void main()
+{
+    int m=2, n=0,num;
+   
+    printf("enter the number:");
+    scanf("%d",&num);
+    
+    if(num==m*n)
+    {
+        printf("yes");
+    }else{
+        printf("no");
+    }
+    getch();
+}
