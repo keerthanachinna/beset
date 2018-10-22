02aee9de94aeb089bdad083b18b68d45055a965c
keerthanachinna <40225685+keerthanachinna@users.noreply.github.com>
2018-10-22 12:30:29 +0530

    Create composite.c

---
+++ composite.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n,fact;
+clrscr();
+printf("enter the number n:");
+scanf("%d",&n);
+for(i=1;i<n;i++)
+{
+if(n%i==0)
+{
+fact==1
+}
+}
+if(fact>1){
+printf("yes");
+}else{
+printf("no");
+}getch();
+}
